a4754798613df6ade94846d7e4466fed7f0d1452
chenjiehao <15921458549@163.com>
2020-12-21 14:20:40 +0800

    hello world

---
+++ src/main/java/com/jh/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.jh.test;
+
+/**
+ * @program: test
+ * @description: 测试
+ * @author: bilusi
+ * @create: 2020-12-21 14:19
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
